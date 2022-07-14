f056897924cdc5e5616aff6ae4ca2813476504ac
iamseungwoo <mongoose@yu.ac.kr>
2022-07-14 18:57:05 +0900

    seung-u like post

--- SeeRoom/review/models.py
+++ SeeRoom/review/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 # Create your models here.
 class ReviewTest(models.Model):
-    contents = models.TextField()
+    contents = models.TextField(null=True, default='')
+    recommend = models.IntegerField(default=0)
+    # buildings -> Foreign key
--- SeeRoom/review/views.py
+++ SeeRoom/review/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import reviewSerialize, reviewDetailSerialize
+from .models import ReviewTest
 
-# Create your views here.
+
+class reviewPostAndList(generics.ListCreateAPIView):
+    queryset = ReviewTest.objects.all()
+    serializer_class = reviewSerialize
+
+class reviewDetail(generics.RetrieveUpdateAPIView):
+    queryset = ReviewTest.objects.all()
+    serializer_class = reviewDetailSerialize
+
+    
+
