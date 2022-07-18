fce767639a0b640ebbaa02ef6c8bc1bf88d05d0a
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-07-18 15:21:36 +0900

    model 수정

--- SeeRoom/review/models.py
+++ SeeRoom/review/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from accounts.models import User
 # Create your models here.
 # class ReviewTest(models.Model): 이부분 없애 주세요
 #     contents = models.TextField(null=True, default='')
@@ -44,29 +45,35 @@ class SafetyEvaluation(models.Model):
     courierBox = models.BooleanField    # 무인택배함 유무
     buildingId = models.ForeignKey(Building, on_delete=models.CASCADE)   # 건물 id
 
+#리뷰
 class ReviewTest(models.Model):
-    contents = models.TextField(null=True, default='')  # 주관식
+    contents = models.TextField(null=True,blank=True, default='')  # 주관식
     recommend = models.PositiveIntegerField(default=0)  # 추천수
     buildingId = models.ForeignKey(Building, on_delete=models.CASCADE, null=True)   # 건물 id
+    userId = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자 id
+    
+    # name = models.CharField(max_length=200, default='') # 이름
+    # address = models.CharField(max_length=200, default='')  # 주소
+    # deposit = models.IntegerField(default=0)   # 보증금
+    # monthlyRent = models.IntegerField(default=0)   # 월세
 
-    name = models.CharField(max_length=200, default='') # 이름
-    address = models.CharField(max_length=200, default='')  # 주소
-    deposit = models.IntegerField(default=0)   # 보증금
-    monthlyRent = models.IntegerField(default=0)   # 월세
+    # moldScore = models.IntegerField(default=0)   # 곰팡이 점수
+    # bugScore = models.IntegerField(default=0)    # 벌레 점수
+    # smellScore = models.IntegerField(default=0)  # 냄새 점수
 
-    moldScore = models.IntegerField(default=0)   # 곰팡이 점수
-    bugScore = models.IntegerField(default=0)    # 벌레 점수
-    smellScore = models.IntegerField(default=0)  # 냄새 점수
+    # internalNoiseScore = models.IntegerField(default=0)  # 내부소음 점수
+    # externalNoiseScore = models.IntegerField(default=0) # 외부소음 점수
+    # floorNoiseScore = models.IntegerField(default=0)  # 층간소음 점수
 
-    internalNoiseScore = models.IntegerField(default=0)  # 내부소음 점수
-    externalNoiseScore = models.IntegerField(default=0) # 외부소음 점수
-    floorNoiseScore = models.IntegerField(default=0)  # 층간소음 점수
+    # parkingScore = models.IntegerField(default=0)    # 주차 점수
+    # managementScore = models.IntegerField(default=0) # 관리 점수
+    # constructionScore = models.IntegerField(default=0)   # 구축/신축 점수
+    # elevator = models.BooleanField(default=False)  # 엘리베이터 유무 
 
-    parkingScore = models.IntegerField(default=0)    # 주차 점수
-    managementScore = models.IntegerField(default=0) # 관리 점수
-    constructionScore = models.IntegerField(default=0)   # 구축/신축 점수
-    elevator = models.BooleanField(default=False)  # 엘리베이터 유무 
+    # femaleOnly = models.BooleanField(default=False)    # 여성전용 여부
+    # cctv = models.BooleanField(default=False)  # cctv 유무
+    # courierBox = models.BooleanField(default=False)   # 무인택배함 유무
 
-    femaleOnly = models.BooleanField(default=False)    # 여성전용 여부
-    cctv = models.BooleanField(default=False)  # cctv 유무
-    courierBox = models.BooleanField(default=False)   # 무인택배함 유무
+class Like(models.Model):
+    buildingId = models.ForeignKey(Building, on_delete=models.CASCADE)   # 건물 id
+    userId = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자 id
--- SeeRoom/review/serializers.py
+++ SeeRoom/review/serializers.py
@@ -4,8 +4,8 @@ from .models import ReviewTest
 class reviewSerialize(serializers.ModelSerializer):
     class Meta:
         model = ReviewTest
-        fields = ('contents', 'buildingId', 'deposit', 'monthlyRent', 'moldScore', 'bugScore', 'smellScore', 'internalNoiseScore', 'externalNoiseScore', 'floorNoiseScore', 'parkingScore', 'managementScore', 'constructionScore', 'elevator', 'femaleOnly', 'cctv', 'courierBox')
-
+        fields = "__all__"
+        
 class reviewDetailSerialize(serializers.ModelSerializer):
     class Meta:
         model = ReviewTest
