2ed838cc612d951a84ce96531e36caf018e0d0d5
nkyungm <skarudals27@naver.com>
2022-07-23 03:41:57 +0900

    경민님

--- sr_react/src/App.js
+++ sr_react/src/App.js
@@ -1,66 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddReview from "./components/ReviewPage/ReviewRegistrationPage";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// import CSRFToken from "./csrftoken";
 import Login from "./components/UserPage/loginPage";
 import SignUp from "./components/UserPage/signupPage";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/sign-in"}></Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/review" element={<AddReview />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-=======
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import Login from "./components/UserPage/loginPage";
-import SignUp from "./components/UserPage/signupPage";
-import Home from './components/Home';
+import Home from "./components/Home";
 import Create from "./components/Building/Create";
-import BuildList from './components/Building/BuildList';
+import BuildList from "./components/Building/BuildList";
 import BdDtail from "./components/Building/BdDtail";
 
 function App() {
@@ -99,19 +46,18 @@ function App() {
           </div>
         </nav>
 
-        
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/create" element={<Create/>}/>
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/list" element={<BuildList />} />
-              <Route path="/list/:ID" element={<BdDtail />} />
-            </Routes>
-          </div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/sign-in" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/list" element={<BuildList />} />
+          <Route path="/list/:ID" element={<BdDtail />} />
+          <Route path="/review" element={<AddReview />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
->>>>>>> 8d06fb649f15e5114137836552375d271a4b398c
--- sr_react/src/components/UserPage/loginPage.js
+++ sr_react/src/components/UserPage/loginPage.js
@@ -1,128 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-
-export const Login = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { replace } = useNavigate();
-
-  const onChangeUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const checkUser = () => {
-    if (username === "" || email === "" || password === "") {
-      alert("이름, 이메일, 비밀번호를 입력해주세요");
-      return;
-    }
-    axios
-      .post("http://127.0.0.1:8000/accounts/login/", {
-        username: username,
-        email: email,
-        password: password,
-      })
-      .then(() => {
-        alert("로그인 성공");
-        console.log("Well done");
-        //console.log("User token", response.data.jwt);
-        //localStorage.setItem("token", response.data.jwt);
-        replace("/"); //페이지 이동 함수
-      })
-      .catch((error) => {
-        alert("로그인 실패");
-        console.log("An error occured:", error.response);
-      });
-  };
-
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      replace("/"); //로그인 페이지안에서 render,다시 로그인으로 돌아올 일 없게 함
-    }
-  }, []);
-
-  return (
-    <form>
-      <h3>Sign In</h3>
-      <div className="mb-3">
-        <label>UserName</label>
-        <input
-          type="string"
-          className="form-control"
-          placeholder="UserName"
-          required
-          //readOnly="false"
-          label="Username"
-          value={username}
-          onChange={onChangeUsername}
-        />
-      </div>
-
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          required
-          //readOnly="false"
-          label="Email"
-          value={email}
-          onChange={onChangeEmail}
-        />
-      </div>
-
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="string"
-          className="form-control"
-          placeholder="Enter password"
-          required
-          //readOnly="false"
-          label="Password"
-          value={password}
-          onChange={onChangePassword}
-        />
-      </div>
-
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
-        </div>
-      </div>
-
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary" onClick={checkUser}>
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
-    </form>
-  );
-};
-
-export default Login;
-=======
 import React, { Component } from "react";
 import axios from "axios";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -132,54 +7,52 @@ const Login = () => {
   return (
     <div className="auth-wrapper">
       <div className="auth-inner">
+        <form>
+          <h3>Sign In</h3>
 
-    <form>
-      <h3>Sign In</h3>
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+            />
+          </div>
 
-      <div className="mb-3">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-        />
-      </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
 
-      <div className="mb-3">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
 
-      <div className="mb-3">
-        <div className="custom-control custom-checkbox">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            Remember me
-          </label>
-        </div>
-      </div>
-
-      <div className="d-grid">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </div>
-      <p className="forgot-password text-right">
-        Forgot <a href="#">password?</a>
-      </p>
-    </form>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
       </div>
     </div>
   );
 };
 
 export default Login;
->>>>>>> 8d06fb649f15e5114137836552375d271a4b398c
