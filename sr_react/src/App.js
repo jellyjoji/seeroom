bba8d88afbb97e5c04d89108f7bb1f6f58dd098e
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-07-23 03:14:00 +0900

    dsf

--- sr_react/src/App.js
+++ sr_react/src/App.js
@@ -1,61 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AddReview from "./components/ReviewPage/ReviewRegistrationPage";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// import CSRFToken from "./csrftoken";
-import Login from "./components/UserPage/loginPage";
-import SignUp from "./components/UserPage/signupPage";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/sign-in"}></Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-up"}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/review" element={<AddReview />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-=======
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 import Login from "./components/UserPage/loginPage";
 import SignUp from "./components/UserPage/signupPage";
 import Home from './components/Home';
@@ -107,6 +54,7 @@ function App() {
               <Route path="/sign-up" element={<SignUp />} />
               <Route path="/list" element={<BuildList />} />
               <Route path="/list/:ID" element={<BdDtail />} />
+              <Route path="/review" element={<AddReview />} />
             </Routes>
           </div>
     </Router>
@@ -114,4 +62,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 8d06fb649f15e5114137836552375d271a4b398c
