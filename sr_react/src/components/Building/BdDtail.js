9dee748e13f8357d13e41641daeb2853a5925ade
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-07-23 05:41:19 +0900

    ff

--- sr_react/src/components/Building/BdDtail.js
+++ sr_react/src/components/Building/BdDtail.js
@@ -2,6 +2,18 @@ import React from 'react';
 import "../../App.css";
 
 const BdDtail = () => {
+    const Params = useParams();
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/reviews/building/${Params.BdID}`).then((response) => {
+          console.log(response);
+          setPost(response.data);
+          setPostLoading(false);
+          setRepls(response.data.repls);
+          setReplLoading(false);
+          replInput.current.focus();
+        });
+      }, []);
+    
     return (
         <div className='component-wrapper'>
             디테이린아러ㅣㄴ어래ㅣㄷ
