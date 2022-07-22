617aa9ff1ca9d89bc1514a2ee91272f5cb6b7103
minsol1 <81753858+minsol1@users.noreply.github.com>
2022-07-23 06:18:51 +0900

    ff

--- sr_react/src/components/Building/BdDtail.js
+++ sr_react/src/components/Building/BdDtail.js
@@ -2,17 +2,17 @@ import React from 'react';
 import "../../App.css";
 
 const BdDtail = () => {
-    const Params = useParams();
-    useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/reviews/building/${Params.BdID}`).then((response) => {
-          console.log(response);
-          setPost(response.data);
-          setPostLoading(false);
-          setRepls(response.data.repls);
-          setReplLoading(false);
-          replInput.current.focus();
-        });
-      }, []);
+    //const Params = useParams();
+    // useEffect(() => {
+    //     axios.get(`http://127.0.0.1:8000/reviews/building/${Params.BdID}`).then((response) => {
+    //       console.log(response);
+    //       setPost(response.data);
+    //       setPostLoading(false);
+    //       setRepls(response.data.repls);
+    //       setReplLoading(false);
+    //       replInput.current.focus();
+    //     });
+    //   }, []);
     
     return (
         <div className='component-wrapper'>
