5525e5345afc8482a65aa6deb41db770689f90b2
jelly <juicy_joji@yu.ac.kr>
2022-07-23 05:14:12 +0900

    3rd

--- sr_react/src/components/Building/EachBd.js
+++ sr_react/src/components/Building/EachBd.js
@@ -7,12 +7,16 @@ const EachBd = ({ID, name, monthlyRent, avg}) => {
         navigate(`${"/list/" + ID}`);
       };
     return (
-        <div onClick={goPost}>
-            <span>건물명 : {name}</span>
-            <span>월세 : {monthlyRent}</span>
-            <span>전체평점  {avg}</span>
-            {/* <span>필터적용 : {e.id}</span> */}
+        <div>
+            <div className='listCards' onClick={goPost}>
+                <span className='cardTitle'>건물명 : {name}</span>
+                <span>월세 : {monthlyRent}</span>
+                <span>전체평점  {avg}</span>
+                {/* <span className="filter">필터적용 : {e.id}</span> */}
+            </div>
+            {/* <div className="filter"></div> */}
         </div>
+        
     );
 };
 
